97ada3cd5854d94356b82c4d0491cd66e9b86888
SuXinDe <suxinde2009@126.com>
2021-09-29 19:34:18 +0800

    update

---
+++ SocketLib/SocketLib.h
@@ -0,0 +1,18 @@
+//
+//  SocketLib.h
+//  SocketLib
+//
+//  Created by SuXinDe on 2021/9/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocketLib.
+FOUNDATION_EXPORT double SocketLibVersionNumber;
+
+//! Project version string for SocketLib.
+FOUNDATION_EXPORT const unsigned char SocketLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketLib/PublicHeader.h>
+
+
